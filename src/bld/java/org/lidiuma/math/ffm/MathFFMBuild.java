cd516fab5828f984174da11c1ec8febfa44fdf13
xasmedy <xasmedy@pm.me>
2026-09-04 17:43:22 +0200

    build: added module name

--- src/bld/java/org/lidiuma/math/ffm/MathFFMBuild.java
+++ src/bld/java/org/lidiuma/math/ffm/MathFFMBuild.java
@@ -11,14 +11,12 @@ public class MathFFMBuild extends BaseProject {
     public MathFFMBuild() {
         pkg = "org.lidiuma.math.ffm";
         name = "MathFFM";
-        mainClass = "org.lidiuma.math.ffm.MathFFM";
+        module = "lidiuma.math.ffm";
         version = version(0,1,0);
         javaRelease = 25;
 
         downloadSources = true;
         repositories = List.of(MAVEN_CENTRAL);
-
-        testOperation().mainClass("org.lidiuma.math.ffm.MathFFMTest");
 
         scope(Scope.compile).include(module("org.lidiuma.math", "math-api", version(1, 0, 0, "rc3")));
     }
